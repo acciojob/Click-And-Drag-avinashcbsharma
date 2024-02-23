1d1fcc53b6fe90cc4198283e84e6bc2953946460
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-02-23 21:59:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// Your code here.
+const items = document.querySelector('.items');
+    let isMouseDown = false;
+    let startX, scrollLeft;
+
+    items.addEventListener('mousedown', (e) => {
+      isMouseDown = true;
+      startX = e.pageX - items.offsetLeft;
+      scrollLeft = items.scrollLeft;
+      items.classList.add('active');
+    });
+
+    items.addEventListener('mouseleave', () => {
+      isMouseDown = false;
+      items.classList.remove('active');
+    });
+
+    items.addEventListener('mouseup', () => {
+      isMouseDown = false;
+      items.classList.remove('active');
+    });
+
+    items.addEventListener('mousemove', (e) => {
+      if (!isMouseDown) return;
+      e.preventDefault();
+      const x = e.pageX - items.offsetLeft;
+      const walk = (x - startX) * 3; // Adjust the speed of scrolling
+
+      items.scrollLeft = scrollLeft - walk;
+    });
